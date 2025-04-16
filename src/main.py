70e9ace6e48a9a9b1432a9eef53217982b542cc0
gunesaydin <gunes.a@gmail.com>
2025-04-16 12:05:16 +0300

    finish site generator logic

--- src/main.py
+++ src/main.py
@@ -1,7 +1,68 @@
+import os
+import shutil
+
 from textnode import TextNode, TextType
+from markdown_blocks import markdown_to_html_node, extract_title
 
 def main():
-    dummy_node = TextNode("dummy text", TextType.LINK, "https://boot.dev")
+    dummy_node = TextNode("****** RUNNING ******", TextType.LINK, "https://boot.dev")
     print(dummy_node)
+    copy_content_from_to("./static", "./public")
+    generate_pages_recursive('./content', './template.html', './public')
+
+def copy_content_from_to(src_path, dest_path):
+    if not os.path.exists(src_path):
+        print("ERROR: Source directory does not exist!")
+        return
+    if os.path.exists(dest_path):
+        print(f"Removing: {dest_path}")
+        shutil.rmtree(dest_path)
+    print(f"Creating New: {dest_path}")
+    os.mkdir(dest_path)
+
+    source_files = os.listdir(src_path)
+    print(f"Source Files ({src_path}): {source_files}")
+    print(f"Copying files from: ({src_path}, to: ({dest_path}))")
+    for path in source_files:
+        print(f"PATH: {path}")
+        if os.path.isfile(os.path.join(src_path, path)):
+            print(f"FILE: {path}")
+            shutil.copy(os.path.join(src_path, path), os.path.join(dest_path, path))
+        else:
+            print(f"DIR: {path}")
+            copy_content_from_to(os.path.join(src_path, path), os.path.join(dest_path, path))
+        
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    markdown = ""
+    html_template = ""
+    with open(from_path, 'r') as file:
+        markdown = file.read()
+    with open(template_path, 'r') as file:
+        html_template = file.read()
+    content = markdown_to_html_node(markdown).to_html()
+    title = extract_title(markdown)
+    site_html = html_template.replace("{{ Title }}", title).replace("{{ Content }}", content)
+    
+    paths = dest_path.split("/")
+    current_path = ""
+    for i in range(0, len(paths) - 1):
+        current_path = os.path.join(current_path, paths[i])
+        if not os.path.exists(current_path):
+            os.mkdir(current_path)
+
+    with open(dest_path, 'a') as file:
+        file.write(site_html)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    source_files = os.listdir(dir_path_content)
+    for path in source_files:
+        print(f"PATH: {path}")
+        if os.path.isfile(os.path.join(dir_path_content, path)):
+            if path.split('.')[1] == 'md':
+                generate_page(os.path.join(dir_path_content, path), template_path, os.path.join(dest_dir_path, f"{path.split('.')[0]}.html"))
+        else:
+            print(f"DIR: {path}")
+            generate_pages_recursive(os.path.join(dir_path_content, path), template_path, os.path.join(dest_dir_path, path))
 
 main()
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,36 +1,113 @@
 import unittest
+from htmlnode import LeafNode, ParentNode, HTMLNode
 
-from htmlnode import HTMLNode
 
 class TestHTMLNode(unittest.TestCase):
-    def test_eq(self):
-        node1 = HTMLNode("<p>", "Test paragraph", props={"test_prop": "test_value"})
-        node2 = HTMLNode("<p>", "Test paragraph", props={"test_prop": "test_value"})
-        self.assertEqual(node1, node2)
+    def test_to_html_props(self):
+        node = HTMLNode(
+            "div",
+            "Hello, world!",
+            None,
+            {"class": "greeting", "href": "https://boot.dev"},
+        )
+        self.assertEqual(
+            node.props_to_html(),
+            ' class="greeting" href="https://boot.dev"',
+        )
 
-        node3 = HTMLNode("<ul>", children=[HTMLNode("<ol>", "Item")])
-        node4 = HTMLNode("<ul>", children=[HTMLNode("<ol>", "Item")])
-        self.assertEqual(node3, node4)
+    def test_values(self):
+        node = HTMLNode(
+            "div",
+            "I wish I could read",
+        )
+        self.assertEqual(
+            node.tag,
+            "div",
+        )
+        self.assertEqual(
+            node.value,
+            "I wish I could read",
+        )
+        self.assertEqual(
+            node.children,
+            None,
+        )
+        self.assertEqual(
+            node.props,
+            None,
+        )
 
-    def test_not_eq(self):
-        node1 = HTMLNode("<p>", "Test paragraph", props={"test_prop": "test_value"})
-        node2 = HTMLNode("<p>", "Test paragraph", props={"test_prop": "test_value2"})
-        self.assertNotEqual(node1, node2)
+    def test_repr(self):
+        node = HTMLNode(
+            "p",
+            "What a strange world",
+            None,
+            {"class": "primary"},
+        )
+        self.assertEqual(
+            node.__repr__(),
+            "HTMLNode(p, What a strange world, children: None, {'class': 'primary'})",
+        )
 
-        node3 = HTMLNode("<ul>", children=[HTMLNode("<ol>", "Item")])
-        node4 = HTMLNode("<ul>", children=[HTMLNode("<ol>", "Item2")])
-        self.assertNotEqual(node3, node4)
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
 
-    def test_prop_to_html(self):
-        node1 = HTMLNode("<p>", "Test paragraph", props={"test_prop": "test_value"})
-        html_with_props1 = node1.props_to_html()
-        control_html1 = f" test_prop=\"test_value\""
-        self.assertEqual(html_with_props1, control_html1)
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://www.google.com">Click me!</a>',
+        )
 
-        node2 = HTMLNode("<p>", "Test paragraph", props={"test_prop": "test_value", "test_prop_2": "test_value_2"})
-        html_with_props2 = node2.props_to_html()
-        control_html2 = " test_prop=\"test_value\" test_prop_2=\"test_value_2\""
-        self.assertEqual(html_with_props2, control_html2)
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+
+    def test_to_html_many_children(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+        self.assertEqual(
+            node.to_html(),
+            "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>",
+        )
+
+    def test_headings(self):
+        node = ParentNode(
+            "h2",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+        self.assertEqual(
+            node.to_html(),
+            "<h2><b>Bold text</b>Normal text<i>italic text</i>Normal text</h2>",
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
